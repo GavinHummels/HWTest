f0db6bd41acc42d6cc797cb087abf1c3226847a4
GavinHummels <1196529490@qq.com>
2017-07-22 16:25:31 +0800

    permtest

--- src/cn/edu/seu/Main.java
+++ src/cn/edu/seu/Main.java
@@ -1,10 +1,12 @@
 package cn.edu.seu;
 
-import java.util.LinkedList;
-import java.util.Scanner;
-import java.util.Stack;
+import java.util.*;
 
 public class Main {
+
+    private static List<String> permList = new ArrayList<>();
+
+    private static List<int[]> permIntList = new ArrayList<>();
 
     private static boolean isLegal(int[] in, int[] out, int n) {
         LinkedList<Integer> stack = new LinkedList<Integer>();
@@ -39,6 +41,68 @@ public class Main {
             }
         }
         return true;
+    }
+
+    public static void permtest(String prefix, String source) {
+        if (source.length() == 0) {
+            permList.add(prefix);
+        } else {
+            for (int i = 0; i < source.length(); i++) {
+                String prefixTemp = prefix + source.charAt(i);
+                String sourceTemp = source.substring(0, i) + source.substring(i + 1, source.length());
+                permtest(prefixTemp, sourceTemp);
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+//        permtest("", "abcd");
+//        System.out.println(permList.size());
+//        for (String str : permList) {
+//            System.out.println(str);
+//        }
+
+        int[] source = {1, 2, 3, 4};
+        permtest(source, 0, 4);
+//        System.out.println(permIntList.size());
+//        for (int[] ints : permIntList) {
+//            for(int i:ints){
+//                System.out.print(i);
+//            }
+//            System.out.print("\n");
+//        }
+    }
+
+    public static void permtest(int[] result, int start, int n) {
+        if (start == n) {
+            return;
+        }
+        if (start == n - 1) {
+            int[] temp = result.clone();
+            permIntList.add(temp);
+            for(int i:temp){
+                System.out.print(i);
+            }
+            System.out.print("\n");
+            return;
+        }
+        for (int i = start; i < n; i++) {
+            swap(result, start, i);
+            permtest(result, start + 1, n);
+            swap(result, start, i);
+        }
+    }
+
+    public static void swap(int[] array, int i, int j) {
+        int temp = array[i];
+        array[i] = array[j];
+        array[j] = temp;
+    }
+
+    public static void validStack(int[] input,int[] output) {
+        Stack<Integer> stack = new Stack<>();
+
+        stack.size();
     }
 
 }
